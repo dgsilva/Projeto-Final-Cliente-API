7207d6328bcaf10b680fefb9be221c12a34e10c9
dgsilva <diegorj93@gmail.com>
2022-11-26 11:59:27 -0300

    criação do projeto cliente.

---
+++ src/main/java/com/projetofinal/api/entities/Cliente.java
@@ -0,0 +1,24 @@
+package com.projetofinal.api.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "cliente")
+public class Cliente {
+
+	@Id
+	private String _idCliente;
+
+	private String nome;
+	@Indexed(unique = true)
+	private String telefone;
+	@Indexed(unique = true)
+	private String email;
+
+	private String senha;
+
+}
