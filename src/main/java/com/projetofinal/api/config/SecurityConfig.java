b5708d7611fad35c94658e1dba07b0f0d18ae99e
dgsilva <diegorj93@gmail.com>
2022-11-26 16:52:51 -0300

    Implementado Swagger e inicio de segurança

---
+++ src/main/java/com/projetofinal/api/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.projetofinal.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.projetofinal.api.filter.TokenAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().addFilterAfter(new TokenAuthenticationFilter(), 
+UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/v1/auth").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/v1/cliente").permitAll()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.anyRequest()
+				.authenticated();
+		
+		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().antMatchers(SWAGGER);
+	}
+	
+	private static final String[] SWAGGER 
+	= { "/v2/api-docs", "/swagger-resources", "/swagger-resources/**",
+				"/configuration/ui", "/configuration/security", "/swagger-ui.html", 
+	"/webjars/**", "/v3/api-docs/**",
+				"/swagger-ui/**" };
+		
+	
+}
